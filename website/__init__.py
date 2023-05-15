e1e6c3f5c6159dc997696029eeab58510a626958
carloslockward <carlos.lockward@gmail.com>
2023-05-15 18:37:51 -0400

    Add google oauth functionality

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask import Flask
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+USE_GOOGLE_AUTH = True
+GOOGLE_CLIENT_ID = "Your google client_id here!"
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "test-key"
+    app.config["SECRET_KEY"] = "Your secret key here!"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+
+    from .models import User
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
---
+++ website/models.py
@@ -0,0 +1,13 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    g_id = db.Column(db.String(150), unique=True)
+    picture_url = db.Column(db.String(2048))
